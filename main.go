c84958b10d41ba16c8f64066ffeb8a84d1269d9b
Daniel T. Morgan <dtmorgan@fields-group.com>
2017-01-16 13:30:08 +0000

    Trying to auto kudos

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
     "github.com/go-vgo/robotgo"
+    "time"
 )
 
 func main() {
+
+  robotgo.MoveMouseSmooth(800, 67, 1.0, 100.0)
   robotgo.MoveMouseSmooth(250, 10, 1.0, 100.0)
   robotgo.MouseClick("left",false)
   robotgo.KeyTap("t", "control")
   robotgo.MoveMouseSmooth(130, 67, 1.0, 100.0)
   robotgo.MouseClick("left",false)
-  robotgo.TypeString("https://www.strava.com/dashboard/following/100") //importing "Hello World"
-  robotgo.KeyTap("enter")           //Press "enter"
-} 
+  robotgo.TypeString("https://www.strava.com/dashboard/following/100")
+  robotgo.KeyTap("enter")
+  time.Sleep(10* time.Second)
+  robotgo.MoveMouseSmooth(200, 67, 1.0, 100.0)
+  robotgo.MouseClick("left",false)
+  robotgo.TypeString("javascript:jQuery(document).ready( function($){ $('.js-add-kudo').each(function(){ $(this).click() }); });")
+  robotgo.KeyTap("enter")
+
+  } 
